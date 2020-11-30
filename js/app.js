27160abe8fafa290f9083c961b245fcdc8c6ac3d
re-z <re-z@ukr.net>
2020-11-30 17:31:34 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,98 @@
+// moving dots func
+var firefly = (function(){
+	var count = 5;
+	var speed = 0.04;
+	var maxDirectionDelta = Math.PI / 20;
+	var maxDirectionDeltaDelta = maxDirectionDelta / 20;
+	
+	var viewportWidth = $(window).width() + 100;
+	var viewportHeight = $(window).height() + 100;
+	
+	var $layer = $('.animated-elements-container');
+	var state = {
+		flies : []
+	};
+	
+	var frames = 0;
+	
+	function modifyDirectionDelta(fly) {
+		var old = fly.directionDelta;
+		var to_max = Math.abs(fly.directionDelta) / maxDirectionDelta;
+		var deltaDelta = maxDirectionDeltaDelta * (Math.random() - 0.5 - 0.5 * to_max);
+		if(fly.directionDelta >= 0) {
+			fly.directionDelta += deltaDelta;
+		} else {
+			fly.directionDelta += deltaDelta * (-1);
+		}
+	}
+	
+	function modifyDirection(fly) {
+		fly.direction += fly.directionDelta;
+	}
+	
+	function modifyPosition(fly) {
+		var left = fly.left + Math.cos(fly.direction) * speed;
+		var top = fly.top + Math.sin(fly.direction) * speed;
+		
+		if(left < 0) { left = 100; }
+		else if(left > 100) { left = 0; }
+		
+		if(top < 0) { top = 100; }
+		else if(top > 100) { top = 0; }
+		
+		fly.left = left;
+		fly.top = top;
+	}
+	
+	function render() {
+		frames++;
+		for(var i = 0; i < state.flies.length; i++) {
+			modifyDirectionDelta(state.flies[i]);
+			modifyDirection(state.flies[i]);
+			modifyPosition(state.flies[i]);
+			
+			state.flies[i].$element.css({
+				transform : 'translate3d(' +
+					(viewportWidth * state.flies[i].left / 100) + 'px, ' +
+					(viewportHeight * state.flies[i].top / 100) + 'px, 0)'
+			});			
+			
+		}
+		window.requestAnimationFrame(render);
+	}
+	
+	function createFly() {
+		var $element = $('<div class="fly-wrap"><div class="fly"></div>');
+		$layer.append($element);
+		var fly = {
+			$element : $element,
+			directionDelta : Math.random() * maxDirectionDelta * 2 - maxDirectionDelta,
+			direction : Math.random() * Math.PI * 2,
+			left : Math.random() * 100 - 50,
+			top : Math.random() * 100 - 50
+		}
+		state.flies.push(fly);
+	}
+
+	function init() {
+		$('body').append($layer);
+		for(var i = 0; i < count; i++) {
+			createFly();
+		}
+		window.requestAnimationFrame(render);
+	}
+
+	return {
+		init : init
+	};
+	
+})();;
+
 $(document).ready(() => {
-    const introTitle = $('.intro__title'),
+    
+    //fullpage plugin
+    if($('#fullpage').fullpage) {
+        const introTitle = $('.intro__title'),
         introText = $('.intro__text'),
         introBtn =  $('.intro__btn'),
         aboutLeft = $('.about__left'),
@@ -8,21 +101,22 @@ $(document).ready(() => {
         servicesRight =  $('.services__right'),
         casesLeft = $('.cases__left'),
         casesRight = $('.cases__right'),
-        careerTitle = $('.career__title'),
-        careerText = $('.career__text'),
-        careerBox = $('.career__box'),
+        careerContainer = $('.career__container'),
         contactsLeft = $('.contacts__left'),
         contactsRight = $('.contacts__right');
 
-    //fullpage plugin
-    if($('#fullpage').fullpage) {
+        $('.header__link').on('click', function() {
+            $(this).siblings().removeClass('is-active').end().addClass('is-active');
+        })
+
         $('#fullpage').fullpage({
             licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
             css3: true,
             scrollingSpeed: 700,
             fixedElements: '.header',
             responsiveWidth: 1024,
-            
+            anchors: ['intro', 'about-us', 'services', 'cases', 'technology', 'career', 'contacts-us'],
+        
             onLeave: function(origin, destination, direction){
                 var leavingSection = this;
                 //intro screen
@@ -66,14 +160,10 @@ $(document).ready(() => {
                     }             
                     //end cases start career
                     if(origin.index == 5){
-                        careerTitle.removeClass(' animate__fadeInDown')
-                        careerText.removeClass(' animate__fadeInDown')
-                        careerBox.removeClass(' animate__fadeInDown')
+                        careerContainer.removeClass(' animate__fadeInDown')
                     } 
                     if(destination.index == 5) {
-                        careerTitle.addClass('animate__fadeInDown')
-                        careerText.addClass('animate__fadeInDown')
-                        careerBox.addClass('animate__fadeInDown')
+                        careerContainer.addClass('animate__fadeInDown')
                     } 
                     //end career start contacts
                     if(origin.index == 6){
@@ -86,9 +176,12 @@ $(document).ready(() => {
     
                     } 
                 }
-                
-                
-            }
+            },
+            afterLoad:  function(origin, destination, direction) {
+                const sectionId = destination.anchor,
+                    linkEl = $(`.header__link[data-anchor=${sectionId}]`);
+                    linkEl.siblings().removeClass('is-active').end().addClass('is-active');
+                }
         });
     }
     
@@ -125,127 +218,13 @@ $(document).ready(() => {
         $('.header__menu').addClass('is-active');
         $('body').addClass('no-scroll');
     })
-    $('.header__close').on('click', function() {
+    $('.header__close, .header__link').on('click', function() {
         $('.header__menu').removeClass('is-active');
         $('body').removeClass('no-scroll');
     })
+
+    //moving dots init
+    firefly.init();
+
 })
-
-
-// playing with code found on http://bucht-der-traeumer.de/
-// I like the animated-elements-container whoever did this code!
-
-var firefly = (function(){
-	
-	var count = 3;
-	var speed = 0.04;
-	var maxDirectionDelta = Math.PI / 20;
-	var maxDirectionDeltaDelta = maxDirectionDelta / 20;
-	
-	var viewportWidth = $(window).width() + 100;
-	var viewportHeight = $(window).height() + 100;
-	
-	var $layer = $('.animated-elements-container');
-	var state = {
-		flies : []
-	};
-	
-	var frames = 0;
-	
-	function modifyDirectionDelta(fly) {
-		var old = fly.directionDelta;
-		var to_max = Math.abs(fly.directionDelta) / maxDirectionDelta;
-		var deltaDelta = maxDirectionDeltaDelta * (Math.random() - 0.5 - 0.5 * to_max);
-		if(fly.directionDelta >= 0) {
-			fly.directionDelta += deltaDelta;
-		} else {
-			fly.directionDelta += deltaDelta * (-1);
-		}
-/*
-		if(Math.abs(fly.directionDelta) > maxDirectionDelta) {
-			console.log(
-				'modifyDirectionDelta',
-				old * (180/Math.PI),
-				maxDirectionDelta * (180/Math.PI),
-				to_max,
-				deltaDelta * (180/Math.PI),
-				fly.directionDelta * (180/Math.PI)
-			);
-		}
-*/
-	}
-	
-	function modifyDirection(fly) {
-		fly.direction += fly.directionDelta;
-	}
-	
-	function modifyPosition(fly) {
-		var left = fly.left + Math.cos(fly.direction) * speed;
-		var top = fly.top + Math.sin(fly.direction) * speed;
-		
-		if(left < 0) { left = 100; }
-		else if(left > 100) { left = 0; }
-		
-		if(top < 0) { top = 100; }
-		else if(top > 100) { top = 0; }
-		
-		fly.left = left;
-		fly.top = top;
-	}
-	
-	function render() {
-		frames++;
-		// if(frames > 200) { return; }
-		for(var i = 0; i < state.flies.length; i++) {
-			modifyDirectionDelta(state.flies[i]);
-			modifyDirection(state.flies[i]);
-			modifyPosition(state.flies[i]);
-			
-/*
-			state.flies[i].$element.css({
-				left : state.flies[i].left + '%',
-				top : state.flies[i].top + '%'
-			});
-*/
-
-			state.flies[i].$element.css({
-				transform : 'translate3d(' +
-					(viewportWidth * state.flies[i].left / 100) + 'px, ' +
-					(viewportHeight * state.flies[i].top / 100) + 'px, 0)'
-			});			
-			
-		}
-		window.requestAnimationFrame(render);
-	}
-	
-	function createFly() {
-		var $element = $('<div class="fly-wrap"><div class="fly"></div>');
-		$layer.append($element);
-		var fly = {
-			$element : $element,
-			directionDelta : Math.random() * maxDirectionDelta * 2 - maxDirectionDelta,
-			direction : Math.random() * Math.PI * 2,
-			left : Math.random() * 100 - 50,
-			top : Math.random() * 100 - 50
-		}
-		state.flies.push(fly);
-	}
-
-	function init() {
-		$('body').append($layer);
-		for(var i = 0; i < count; i++) {
-			createFly();
-		}
-		window.requestAnimationFrame(render);
-	}
-
-	return {
-		init : init
-	};
-	
-})();;
-
-$(document).ready(function() {
-  firefly.init();
-});
 
